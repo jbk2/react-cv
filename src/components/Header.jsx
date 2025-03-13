67651a2f7a3caeb939c9a119315d153ea9d86f66
jbk2 <james@humanbenefits.com>
2025-03-13 17:33:50 +0100

    Styling header and view-toggle selection within it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,10 @@ export default function Header() {
   return (
     <header className="">
       <h2>Curriculum Vitae</h2>
+      <div id="view-toggle">
+        <p className="selected">Edit CV</p>
+        <p>View CV</p>
+      </div>
     </header>
   )
 }
