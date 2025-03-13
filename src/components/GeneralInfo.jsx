82e39b996d2d3a3ca58dc1a2f034d5faefb9a7db
jbk2 <james@humanbenefits.com>
2025-03-13 17:50:27 +0100

    create state for all General Info fields

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,18 +1,56 @@
+import { useState } from "react"
+
 function GeneralInfo() {
-  // section, form with titles and inputs 
-  // name, email, phone, twitter
+  const [ state, setState ] = useState({
+    firstName:"",
+    lastName:"",
+    email:"",
+    telephone:""
+  })
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setState(prevState => ({
+      ...prevState,
+      [id]: value
+    }));
+  }
+
   return(
     <section>
       <h3>General Info</h3>
       <form action="#" className="form-grid">
         <label htmlFor="first-name">First name</label>
-        <input type="text" id="first-name" />
+        <input
+          type="text"
+          id="firstName"
+          value={state.firstName}
+          onChange={handleChange}
+        />
+        
         <label htmlFor="last-name">Last name</label>
-        <input type="text" id="last-name" />
+        <input
+          type="text"
+          id="lastName"
+          value={state.lastName}
+          onChange={handleChange}
+        />
+        
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          id="email"
+          value={state.email}
+          onChange={handleChange}
+        />
+        
         <label htmlFor="telephone">Telephone</label>
-        <input type="text" id="telephone" />
+        <input
+          type="text"
+          id="telephone"
+          value={state.telephone}
+          onChange={handleChange}
+        />
       </form>
       <hr />
     </section>
